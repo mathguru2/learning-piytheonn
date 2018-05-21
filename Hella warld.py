6b9a96de4ac8396132caa5394730c56a82889f68
mathguru2 <jonyarbrough10@yahoo.com>
2018-05-21 10:18:41 -0500

    First assingment the cube function

--- Hella warld.py
+++ Hella warld.py
@@ -1,9 +1,14 @@
 def cube(arg1):
     if int(arg1):
-        arg1=int(arg1)
-        total=arg1*arg1*arg1
+        if int(arg1)==arg1:
+            arg1=int(arg1)
+            total=arg1*arg1*arg1
+        else:
+            print("put in an integer into the cube function")
     else:
         print("put in an integer into the cube function")
 
     return total;
 
+
+print(cube(1.2))
