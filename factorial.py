4a65526643755467c97dc42e58bacaef898391a9
mathguru2 <jonyarbrough10@yahoo.com>
2018-05-22 21:07:07 -0500

    Factorial

--- factorial.py
+++ factorial.py
@@ -1,11 +1,11 @@
 def factorial(arg1):
     tot=1
     if int(arg1)==arg1 and arg1>0:
-        for i in range(arg1):
+        for i in range(1,arg1+1):
             tot=tot*i
         print(tot)
     else:
         print("You need to enter an integer")
 
     return tot;
-factorial(3)
+factorial(2)
